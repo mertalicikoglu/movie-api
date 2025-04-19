bcff2294b5229df6c8b1764a19ffe92f5b267881
MERT ALİCİKOĞLU <alicikoglumert@gmail.com>
2025-04-19 13:29:08 +0300

    refactor server initialization to use async function for database connection and configuration; update port retrieval from config file

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,14 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import config from './infrastructure/config'; // Configuration settings
+import connectDB from './infrastructure/db/connection'; // Database connection function
+
+
 
 const app = express();
+const PORT = config.port; // Get port from config file
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,10 +24,24 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-
-
-export default app;
+// Async function to start the server
+const startServer = async () => {
+    try {
+      // 1. Connect to database
+      await connectDB();
+  
+      // 2. Start Express application
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        console.log(`Access URL: http://localhost:${PORT}`);
+      });
+  
+    } catch (error) {
+      console.error('Failed to start server:', error);
+      process.exit(1); // Exit application if error occurs
+    }
+  };
+  
+  // Start the server
+  startServer();
 
